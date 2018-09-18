ee710823e9079d469b449e0d5d480931d827eda1
pspgbhu <brotherchun001@gmail.com>
2018-09-18 11:20:07 +0800

    test: fix a bug about cache dir cannot be removed

--- test/bin.test.js
+++ test/bin.test.js
@@ -6,23 +6,25 @@ jest.setTimeout(1000 * 30);
 
 const cwd = path.resolve(__dirname, '../');
 
-test('exec download a single file', done => {
-  exec(
-    './bin/repo -r "pspgbhu/github-download-parts" -t __bin_demo__ -p README.md --mock test/mock.json',
-    { cwd },
-    (err, stdout, stderr) => {
-      if (err) {
-        console.log('stdout:', stdout);
-        console.log('stderr:', stderr);
-        console.log('err:', err);
-        throw new Error(err);
-      }
-      expect(fs.existsSync(path.join(cwd, '__bin_demo__', 'README.md'))).toBe(true);
-      done();
-    },
-  );
-});
+describe('CLI Function', () => {
+  test('exec download a single file', done => {
+    exec(
+      './bin/repo -r "pspgbhu/github-download-parts" -t __bin_demo__ -p README.md --mock test/mock.json',
+      { cwd },
+      (err, stdout, stderr) => {
+        if (err) {
+          console.log('stdout:', stdout);
+          console.log('stderr:', stderr);
+          console.log('err:', err);
+          throw new Error(err);
+        }
+        expect(fs.existsSync(path.join(cwd, '__bin_demo__', 'README.md'))).toBe(true);
+        done();
+      },
+    );
+  });
 
-afterAll(() => {
-  fs.remove(path.join(cwd, '__bin_demo__'));
+  afterAll(() => {
+    fs.remove(path.join(cwd, '__bin_demo__'));
+  });
 });
